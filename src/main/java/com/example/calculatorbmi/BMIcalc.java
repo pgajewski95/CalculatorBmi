f9a9baeb4ee6f1e50184b5b3ddf02b363e7a4406
pgajewski95 <pgajewski95@wp.pl>
2019-03-31 15:04:16 +0200

    add describrion

--- src/main/java/com/example/calculatorbmi/BMIcalc.java
+++ src/main/java/com/example/calculatorbmi/BMIcalc.java
@@ -22,8 +22,24 @@ public class BMIcalc {
         double factor2 = unitweight.equals("kg") ? 1 : 2.20462262 ;
 
         Double bmi = (weight/factor2) / (height * height * 0.0001 *factor1*factor1);
+        if (bmi<16){
+            return String.format("%.1f", bmi)+"     " + "wygłodzenie";}
+        else if (bmi>=16 && bmi<17){
+            return String.format("%.1f", bmi)+"     " + "wychudzenie";}
+        else if (bmi>=17 && bmi <18.5){
+            return String.format("%.1f", bmi)+"     " + "niedowaga";}
+        else if(bmi>=18.5 && bmi <25){
+            return String.format("%.1f", bmi)+"     " + "waga prawidłowa";}
+        else if (bmi>=25 && bmi<30){
+            return String.format("%.1f", bmi)+"     " + "nadwaga";}
+        else if (bmi>=30 && bmi<35){
+            return String.format("%.1f", bmi)+"     " + "I Stopien otyłosci";}
+        else if (bmi>=35 && bmi<40){
+            return String.format("%.1f", bmi)+"     " + "II Stopien otyłosci";}
+       else {
+            return String.format("%.1f", bmi) + "     " + "otyłosc skrajna";
+        }
 
-        return String.format("%.1f", bmi);
     }
 
 
